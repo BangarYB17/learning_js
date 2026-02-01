5a3abeef1ae6b7302e0a4ed022bcf55cbf64b786
BangarYB17 <bangaryogeshwar3@gmail.com>
2026-02-01 11:22:11 +0530

    opertors

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function showmessage2(){
+    alert("Hello it's External JS")
+}
+
+document.write("<h1>Welcome To Pune</h1>");
+
+var c_name="Fortune Cloud Pimpri Pune<br>";
+document.write(c_name);
+document.write(c_name);
+document.write(c_name);
+document.write(c_name);
+document.write(c_name);
